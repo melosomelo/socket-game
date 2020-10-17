b9ad423272889b9a50c42fdeda53b2173115423e
Mateus <mateusmelornrn@gmail.com>
2020-10-17 20:03:15 -0300

    Change backend typescript setup

--- socket-game-back/src/index.ts
+++ socket-game-back/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.use((req, res) => {
-  return res.status(200).send(`<h1> Hello world! </h1>`);
+  return res.status(200).send(`<h1> Hello world from node with TS! </h1>`);
 });
 
 app.listen(3000);
