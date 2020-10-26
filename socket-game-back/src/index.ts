bbd3cca268b2203cfa7da27b8b34c4abcc09b34d
Mateus <mateusmelornrn@gmail.com>
2020-10-25 21:29:30 -0300

    Add socketio on server

--- socket-game-back/src/index.ts
+++ socket-game-back/src/index.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import http from "http";
+import socketio from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = socketio(server, {});
+
+io.on("connection", (socket) => {
+  console.log("eai");
+});
 
 app.use((req, res) => {
   return res.status(200).send(`<h1> Hello world from node with TS! </h1>`);
 });
 
-app.listen(3000);
+server.listen(3000);
