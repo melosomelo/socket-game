05e191233b49b4d6a7fc68bd0c4c2ab80524e450
Mateus <mateusmelornrn@gmail.com>
2020-10-29 14:21:40 -0300

    Add Ball styles

---
+++ socket-game-front/src/components/Ball/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Ball = styled.div`
+  width: 42px;
+  height: 42px;
+
+  background-color: white;
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
