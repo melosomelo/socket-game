59cda7d5501a75b3036b8d562e426f00cf2a7556
Mateus <mateusmelornrn@gmail.com>
2020-10-29 11:44:14 -0300

    Setup basic for future Ball movement

--- socket-game-front/src/components/Ball/Ball.tsx
+++ socket-game-front/src/components/Ball/Ball.tsx
@@ -1,16 +1,35 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
 
-const Ball = styled.div`
-  width: 42px;
-  height: 42px;
+import { Ball } from "./styles";
 
-  background-color: white;
+/* When the game first starts, we need to set a few things: 
+  1. The direction the ball will first move (up or down)
+  2. The angle in which the ball will go to that direction. This can be at max 45deg, be that to the left 
+  or right.
+  */
 
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-`;
+type BallDirection = "up" | "down";
 
-export default Ball;
+const BallComponent: React.FC = () => {
+  const [firstDirection, setFirstDirection] = useState<BallDirection>(() => {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.5) {
+      return "down";
+    }
+    return "up";
+  });
+
+  const [angle, setAngle] = useState(() => {
+    //first, right or left?
+    const direction = Math.random() < 0.5 ? "right" : "left";
+    //now, the angle
+    let angle = Math.random() * 45;
+    if (direction === "left") angle *= -1;
+
+    return angle;
+  });
+
+  return <Ball />;
+};
+
+export default BallComponent;
