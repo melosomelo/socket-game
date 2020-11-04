edfbab12736505c4ecfd1af29356ad8bacef5844
Mateus <mateusmelornrn@gmail.com>
2020-11-04 12:15:13 -0300

    gitignore dist folder && change npm start command

--- socket-game-back/dist/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-app.use((req, res) => {
-    return res.status(200).send(`<h1> Hello world! </h1>`);
-});
-app.listen(3000);
-//# sourceMappingURL=index.js.map
